f01e6e924637316f699719dab2aa92304a42ac16
egosti <emily.gosti@gmail.com>
2016-05-12 16:00:18 -0700

    BoggleLetter

---
+++ BoggleLetter.java
@@ -0,0 +1,8 @@
++  CREATE LETTER CLASS
+ +    -boolean: isVowel
+ +    -int x, int y (position on board)
+ +      method: getPosition()
+ +        return int[] with [x, y]
+        method: setPosition()
+ +    -char letter
+ +      method: getLetter()

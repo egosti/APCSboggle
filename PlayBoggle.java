55c4596e1fb72bfb552bb50b16158841ed5fabc4
natasha41575 <nat41575@gmail.com>
2016-05-12 17:09:37 -0700

    Update PlayBoggle.java

--- PlayBoggle.java
+++ PlayBoggle.java
@@ -1,4 +1,30 @@
-then maybe we should create a PlayBoggle class?
+import java.util.*;
+
+public class PlayBoggle{
+  public static void main(String[] args){
+      Scanner c = new Scanner(System.in);
+      BoggleGrid grid = new BoggleGrid(size);
+    /* create timer
+     * display grid
+     * while timer > 0, take input from user getGuesses(c) */
+  }
+  public static void getGuesses(Scanner c){
+    String guess = c.next();
+    guess = guess.toUpperCase();
+    
+      // *** OUTER LOOP*** 
+    char c = guess.charAt(0);
+      /* go through grid & check if any letter matches char c
+       * return position **[r, c]** of letter, otherwise return [-1, -1]
+       
+       * if YES: 
+          checkIfWordMatches(guess, position); */
+  }
+  public static boolean checkIfWordMatches(String guess, int[] position){
+    //recursive function
+  }
+}
+
 
 methods:
 -main: (import list of valid words)

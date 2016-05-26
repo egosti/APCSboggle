e9be9bdbdd2d7705fef5e25c6b8003d93d72f294
natasha41575 <nat41575@gmail.com>
2016-05-25 18:19:32 -0700

    Update PlayBoggle.java

--- PlayBoggle.java
+++ PlayBoggle.java
@@ -1,69 +1,87 @@
 import java.util.*;
 
 public static int score = 0;
+public static BoggleGrid grid;
+public static ArrayList<String> guesses;
 
 public class PlayBoggle{
+  
+  /** Get size from user and create grid of that size.
+   *  Display grid.
+   *  Allow the user to guess as many words as possible.
+   */
   public static void main(String[] args){
-      Scanner c = new Scanner(System.in);
-      BoggleGrid grid = new BoggleGrid(size);
-      grid.printGrid(); //displays grid
-      getGuesses(c, grid);
-    /* create timer
-     * while timer > 0, take input from user getGuesses(c) */
-  }
-  public static void getGuesses(Scanner c, BoggleGrid grid){
-    String guess = c.next();
-    guess = guess.toUpperCase();
+    Scanner c = new Scanner(System.in);
+    int size = c.nextInt();
+    BoggleGrid grid = new BoggleGrid(size);
+    grid.printGrid(); //displays grid
     
-      // *** OUTER LOOP*** 
-    char ch = guess.charAt(0);
-      /* go through grid & check if any letter matches char c
-       * return position **[r, c]** of letter, otherwise return [-1, -1]
-       * also need a way to check the next instance of c if the first one doesn't work
-       
-       * if YES: 
-          checkIfWordMatches(guess, position, 1); */
+    String guess = "";
+    while (!guess.equals(***)){
+      guess = getGuesses(c);
+      guesses.add(guess);
+      System.out.println(guess);
+    }
+  }
+  
+  /** Get guesses from the user.
+   *  Go through entire grid to search for the word.
+   */
+  public static String getGuesses(Scanner c){
+    String guess = c.next();
+    if (guess.equals("***") || alreadyGuessed()) return guess;
+    guess = guess.toUpperCase();
+    //goes through entire grid
     for (int row = 0; row < size; row++) {
       for (int col = 0; col < size; col++) {
-        if (grid[row][col].getLetter() == guess) {
-          if (checkIfWordMatches(guess, position, 1)){
-            score += guess.length();
-            break;
-          } 
+        if (checkIfWordMatches(guess, position, 0)){
+          score += guess.length(); //adds length of guess to the score
+          break;
         }
       }
     }
+    return guess;
   }
-  public static boolean checkIfWordMatches(String guess, int[] position, int i){
-    //base case 1
-    if (i == guess.length()){
-      return true;
-    }
-    //base case 2
-    else if (guess.charAt(i) != /****letter @ grid position*/ ){
-      return false;
-    }
-    //recursive case
-    else {
-      i++; 
-      return 
-      checkIfWordMatches(guess, [r-1, c-1], i) ||
-      checkIfWordMatches(guess, [r-1, c], i) || 
-      checkIfWordMatches(guess, [r-1, c+1], i) ||
-      checkIfWordMatches(guess, [r, c-1], i) ||
-      checkIfWordMatches(guess, [r, c+1], i) ||
-      checkIfWordMatches(guess, [r+1, c-1], i) ||
-      checkIfWordMatches(guess, [r+1, c], i) ||
-      checkIfWordMatches(guess, [r+1, c+1], i);
-    }
-  }
-}
-
-
-methods:
--main: (import list of valid words)
--timer 
--public static boolean guessWord (checks if user input is a solution and hasn't been guessed before)
--addPoint (different point values for words of different lengths)
   
+  /** Recursive function.
+   *  Checks if the word is in the grid; returns false if no. 
+   */
+  public static boolean checkIfWordMatches(String guess, int[] position, int 0){
+    int r = position[0];
+    int c = position[1];
+    
+    //assert that the given position exists in the grid
+    if (r < guess.length() && r >= 0 && c < guess.length() && c >= 0){
+      //base case 1
+      if (i == guess.length()){
+        return true;
+      }
+      //base case 2
+      else if (guess.charAt(i) != grid[r][c].getLetter()){
+        return false;
+      }
+      //recursive case
+      else {
+        i++; 
+        return 
+        checkIfWordMatches(guess, [r-1, c-1], i) ||
+        checkIfWordMatches(guess, [r-1, c], i) || 
+        checkIfWordMatches(guess, [r-1, c+1], i) ||
+        checkIfWordMatches(guess, [r, c-1], i) ||
+        checkIfWordMatches(guess, [r, c+1], i) ||
+        checkIfWordMatches(guess, [r+1, c-1], i) ||
+        checkIfWordMatches(guess, [r+1, c], i) ||
+        checkIfWordMatches(guess, [r+1, c+1], i);
+      }
+    }
+  }
+  public static boolean alreadyGuessed(){
+    for (int i=0; i<guesses.size(); i++){
+      if (guess.equals(guesses.get(i)){
+        System.out.println("Already guessed.");
+        return true;
+      }
+      else return false;
+    }
+  }
 }

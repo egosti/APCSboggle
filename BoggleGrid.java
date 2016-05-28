4a9d6fa42272ecb72b0f52fdcee1c64ed7797369
natasha41575 <nat41575@gmail.com>
2016-05-27 21:40:52 -0700

    Update BoggleGrid.java

--- BoggleGrid.java
+++ BoggleGrid.java
@@ -1,16 +1,12 @@
 /* pseudocode for the Boggle Grid
-
-
 //instance variables:
 2D array 
 list of all possible words
-
 constructor:
 -get input from reader for size of board
 -create a 2D array with a side = input from reader
 -create array of the 26 letters (see letter class)
 -for each spot in array, assign a random letter according to guidelines (create a method)
-
 fillGrid method:
 -fill in vowels first
 -fill in remaining letters randomly, Q can only come in if there's already a U, and Q is limited to the areas around U
@@ -226,10 +222,9 @@ public class BoggleGrid {
     }
     return temp;
   }
+
+  public BoggleLetter getLetterAtPosition(int r, int c){
+    return this.grid[r][c];
+  }
   
 }
-
-
-
-
-

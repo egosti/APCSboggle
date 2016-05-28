e7bb09b5e920d916f89b6eb5d87adce10c8f2793
natasha41575 <nat41575@gmail.com>
2016-05-27 21:41:28 -0700

    Update PlayBoggle.java

--- PlayBoggle.java
+++ PlayBoggle.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
-public static int score = 0;
-public static BoggleGrid grid;
-public static ArrayList<String> guesses;
-
 public class PlayBoggle{
+
+  public static int score = 0;
+  public static BoggleGrid grid;
+  public static ArrayList<String> guesses = new ArrayList<String>();
+  public static int size;
+  public static String guess;
   
   /** Get size from user and create grid of that size.
    *  Display grid.
@@ -13,8 +15,8 @@ public class PlayBoggle{
   public static void main(String[] args){
     Scanner c = new Scanner(System.in);
     System.out.println("Enter size: ");
-    int size = c.nextInt();
-    BoggleGrid grid = new BoggleGrid(size);
+    size = c.nextInt();
+    grid = new BoggleGrid(size);
     System.out.println(grid); //displays grid
     
     String guess = "";
@@ -30,13 +32,14 @@ public class PlayBoggle{
    */
   public static String getGuesses(Scanner c){
     System.out.println("Enter guess: ");
-    String guess = c.next();
+    guess = c.next();
     if (guess.equals("***") || alreadyGuessed()) return guess;
     guess = guess.toUpperCase();
     //goes through entire grid
     for (int row = 0; row < size; row++) {
       for (int col = 0; col < size; col++) {
-        if (checkIfWordMatches(guess, [row, col], 0)){
+        int[] position = new int[2];
+         if (checkIfWordMatches(guess, row, col, 0)){
           score += guess.length(); //adds length of guess to the score
           break;
         }
@@ -48,43 +51,40 @@ public class PlayBoggle{
   /** Recursive function.
    *  Checks if the word is in the grid; returns false if no. 
    */
-  public static boolean checkIfWordMatches(String guess, int[] position, int i){
-    int r = position[0];
-    int c = position[1];
-    
+  public static boolean checkIfWordMatches(String guess, int r, int c, int i){
     //assert that the given position exists in the grid
-    if (r < guess.length() && r >= 0 && c < guess.length() && c >= 0){
+    if (r < size && r >= 0 && c < size && c >= 0){
       //base case 1
       if (i == guess.length()){
         return true;
       }
       //base case 2
-      else if (guess.charAt(i) != grid[r][c].getLetter()){
+      else if (guess.charAt(i) != grid.getLetterAtPosition(r, c).getLetter()){
         return false;
-      }
+      } 
       //recursive case
       else {
         i++; 
         return 
-        checkIfWordMatches(guess, [r-1, c-1], i) ||
-        checkIfWordMatches(guess, [r-1, c], i) || 
-        checkIfWordMatches(guess, [r-1, c+1], i) ||
-        checkIfWordMatches(guess, [r, c-1], i) ||
-        checkIfWordMatches(guess, [r, c+1], i) ||
-        checkIfWordMatches(guess, [r+1, c-1], i) ||
-        checkIfWordMatches(guess, [r+1, c], i) ||
-        checkIfWordMatches(guess, [r+1, c+1], i);
+        checkIfWordMatches(guess, r-1, c-1, i) ||
+        checkIfWordMatches(guess, r-1, c, i) || 
+        checkIfWordMatches(guess, r-1, c+1, i) ||
+        checkIfWordMatches(guess, r, c-1, i) ||
+        checkIfWordMatches(guess, r, c+1, i) ||
+        checkIfWordMatches(guess, r+1, c-1, i) ||
+        checkIfWordMatches(guess, r+1, c, i) ||
+        checkIfWordMatches(guess, r+1, c+1, i);
       }
     }
     else return false;
   }
   public static boolean alreadyGuessed(){
     for (int i=0; i<guesses.size(); i++){
-      if (guess.equals(guesses.get(i)){
+      if (guess.equals(guesses.get(i))){
         System.out.println("Already guessed.");
         return true;
       }
-      else return false;
     }
+    return false;
   }
 }
